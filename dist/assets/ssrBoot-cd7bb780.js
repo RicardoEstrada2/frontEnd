84c5dd8428c9fb52fd3e5512873421da1fdf7d17
RicardoEstrada2 <restrada_05@alu.uabcs.mx>
2023-12-09 17:44:46 -0700

    adding dist

---
+++ dist/assets/ssrBoot-cd7bb780.js
@@ -0,0 +1,1 @@
+import{r as t,aR as e,c as s,b0 as n}from"./index-dfeb318d.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
